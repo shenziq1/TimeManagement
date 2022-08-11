f2249d9a34fba71cd873f055fbf1bf11b0c93e4f
shenziq1 <ziqi.shen@mail.utoronto.ca>
2022-08-11 17:05:45 -0400

    added button

--- app/src/main/java/com/github/shenziq1/timemanagement/MainActivity.kt
+++ app/src/main/java/com/github/shenziq1/timemanagement/MainActivity.kt
@@ -2,10 +2,29 @@ package com.github.shenziq1.timemanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var scoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        scoreTextView = findViewById(R.id.your_score_text_view)
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+        tapMeButton = findViewById(R.id.tap_me_button)
+        tapMeButton.setOnClickListener { incrementScore() }
     }
+
+    private fun incrementScore(){
+        score++
+        val newScore = "Your Score: $score"
+        scoreTextView.text = newScore
+    }
+
+
 }
